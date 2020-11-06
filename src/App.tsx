ac391d2cb4a19ae27cf0a9d96b910a6020f59c4c
David <jobroman83@gmail.com>
2020-11-06 13:16:20 +0100

    [ADD] alertReducer

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import React, { FC } from 'react'
 const App:FC = () => {
     return (
         <div>
-            <h1>Hello world</h1>
+            <h1>Hello world im here</h1>
         </div>
     )
 }
